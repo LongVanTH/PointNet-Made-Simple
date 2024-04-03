639f4f7e510485381056bf431e9a091547b5910c
Long Vân <tranhalongvan@gmail.com>
2024-04-03 00:04:51 -0400

    Initial commit

---
+++ download_dataset.sh
@@ -0,0 +1,4 @@
+gdown 1wXOgwM_rrEYJfelzuuCkRfMmR0J7vLq_
+unzip a5_data.zip
+rm -rf __MACOSX
+rm a5_data.zip
---
+++ models.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# ------ TODO ------
+class cls_model(nn.Module):
+    def __init__(self, num_classes=3):
+        super(cls_model, self).__init__()
+        pass
+
+    def forward(self, points):
+        '''
+        points: tensor of size (B, N, 3)
+                , where B is batch size and N is the number of points per object (N=10000 by default)
+        output: tensor of size (B, num_classes)
+        '''
+        pass
+
+# ------ TODO ------
+class seg_model(nn.Module):
+    def __init__(self, num_seg_classes = 6):
+        super(seg_model, self).__init__()
+        pass
+
+    def forward(self, points):
+        '''
+        points: tensor of size (B, N, 3)
+                , where B is batch size and N is the number of points per object (N=10000 by default)
+        output: tensor of size (B, N, num_seg_classes)
+        '''
+        pass
